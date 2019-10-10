1e397e0fb8cf9393ff92bf77ea258ff40abe88d5
Dillan Teagle <dteagle@cpicardgroup.com>
2019-10-10 17:20:44 -0500

    publish resume to git

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+  
+module.exports = {
+    plugins: [
+      require('precss'),
+      require('autoprefixer'),
+      require('postcss-simple-vars')({
+        variables: require('./src/css/variables.css.js')
+      }),
+    ]
+  }
